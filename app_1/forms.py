2af0a075c8b3b19d2341cd56f903bab1c737ca1c
JuanesPavas19 <jepavasg@eafit.edu.co>
2022-11-22 13:06:14 -0500

    Correcciones

--- app_1/forms.py
+++ app_1/forms.py
@@ -1,10 +1,18 @@
-from pyexpat import model
-from socket import fromshare
 from django import forms
-from .models import Oferta
+from .models import Aspirante, Empresa, Oferta
 
 class ingresarOfertaForm(forms.ModelForm):
     class Meta:
         model = Oferta
-        fields = ["Empresa_id_empresa", "Nombre_empresa", "cargo", "perfil_buscado", "salario"]
-        #fields = '__all__'
+        #fields = ["Empresa_id_empresa", "Nombre_empresa", "cargo", "perfil_buscado", "salario"]
+        fields = '__all__'
+
+class ingresarEmpresaForm(forms.ModelForm):
+    class Meta:
+        model = Empresa
+        fields = '__all__'
+
+class ingresarAspiranteForm(forms.ModelForm):
+    class Meta:
+        model = Aspirante
+        fields = '__all__'
